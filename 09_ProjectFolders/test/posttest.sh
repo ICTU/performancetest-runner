5194d1b033d9f007d9a7106933b021194295b03d
Marcel Wigman <mwigman@ymor.nl>
2019-02-19 16:38:07 +0100

    initialize 09_ProjectFolders

---
+++ 09_ProjectFolders/test/posttest.sh
@@ -0,0 +1,6 @@
+echo "-----------------------------------------------------------------------"
+
+echo "Get application version"
+# applicatieversie=$(curl -s "http://jenkins.lrk.ictu:8080/view/trains/view/train%20perf-jboss/job/train-env-perf-setup-deploy/lastSuccessfulBuild/api/xml?xpath=/*/action/parameter/value/text()")
+
+echo "-----------------------------------------------------------------------"
