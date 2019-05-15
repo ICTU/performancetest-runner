bbf9bd837d540715aaec778cf1272b8271a056e0
Erik Post <epost@ymor.nl>
2019-05-15 10:24:44 +0200

    Added force parameter to copy in rptgen.merge to prevent being asked to overwrite

--- 06_Tools/ReportTool/tools/rptgen.merge.sh
+++ 06_Tools/ReportTool/tools/rptgen.merge.sh
@@ -15,22 +15,22 @@ aborttest() {
 
 echo "GenerateReport - Arrange prerequisites..."
 if [[ ! -e $rpt_reporttemplatefilename ]]; then aborttest "report template not found"; fi
-cp $rpt_reporttemplatefilename $rpt_reporttemptargetfile
-cp $rpt_reporttemplatefilename $rpt_temppath/report.00.html
+cp -f $rpt_reporttemplatefilename $rpt_reporttemptargetfile
+cp -f $rpt_reporttemplatefilename $rpt_temppath/report.00.html
 
 echo "GenerateReport - Merge data with html template..."
-cp $rpt_reporttemplatefilename $rpt_reporttemptargetfile
+cp -f $rpt_reporttemplatefilename $rpt_reporttemptargetfile
 dotnet $rpt_toolspath/rpg.merge.dll i project=$rpt_projectname testrun=$rpt_runname category=msr entity=* templatefile=$rpt_reporttemptargetfile database=$rpt_reportdbconnectstring
 if [ $? -ne 0 ]; then aborttest "merge measurements/msr"; fi
-cp $rpt_reporttemptargetfile $rpt_temppath/report.01.html
+cp -f $rpt_reporttemptargetfile $rpt_temppath/report.01.html
 
 dotnet $rpt_toolspath/rpg.merge.dll it project=$rpt_projectname testrun=$rpt_runname category=trs templatefile=$rpt_reporttemptargetfile database=$rpt_reportdbconnectstring
 if [ $? -ne 0 ]; then aborttest "merge transactiondata/trs"; fi
-cp $rpt_reporttemptargetfile $rpt_temppath/report.02.html
+cp -f $rpt_reporttemptargetfile $rpt_temppath/report.02.html
 
 dotnet $rpt_toolspath/rpg.merge.dll ib project=$rpt_projectname testrun=$rpt_runname category=trs baselinetestrun=$rpt_baselineref templatefile=$rpt_reporttemptargetfile database=$rpt_reportdbconnectstring
 if [ $? -ne 0 ]; then aborttest "merge transaction baselinedata/trs"; fi
-cp $rpt_reporttemptargetfile $rpt_temppath/report.03.html
+cp -f $rpt_reporttemptargetfile $rpt_temppath/report.03.html
 
 echo "GenerateReport - Merge variables with template..."
 dotnet $rpt_toolspath/rpg.merge.dll i project=$rpt_projectname testrun=$rpt_runname category=var templatefile=$rpt_reporttemptargetfile database=$rpt_reportdbconnectstring
@@ -45,24 +45,24 @@ dotnet $rpt_toolspath/rpg.merge.dll v project=$rpt_projectname name=baselineref 
 dotnet $rpt_toolspath/rpg.merge.dll v project=$rpt_projectname name=maxtrendcount value=$rpt_maxtrendcount templatefile=$rpt_reporttemptargetfile database=$rpt_reportdbconnectstring
 dotnet $rpt_toolspath/rpg.merge.dll v project=$rpt_projectname name=trendworkload value=$rpt_workload templatefile=$rpt_reporttemptargetfile database=$rpt_reportdbconnectstring
 dotnet $rpt_toolspath/rpg.merge.dll v project=$rpt_projectname name=companylogoreference value=$rpt_companylogoreference templatefile=$rpt_reporttemptargetfile database=$rpt_reportdbconnectstring
-cp $rpt_reporttemptargetfile $rpt_temppath/report.04.html
+cp -f $rpt_reporttemptargetfile $rpt_temppath/report.04.html
 
 echo "GenerateReport - Cleanup non-used variables top section of the report..."
 dotnet $rpt_toolspath/rpg.merge.dll v project=$rpt_projectname name=* value=" " templatefile=$rpt_reporttemptargetfile beginpattern="Response times (ms)" endpattern="Trend (ms)" database=$rpt_reportdbconnectstring
-cp $rpt_reporttemptargetfile $rpt_temppath/report.05.html
+cp -f $rpt_reporttemptargetfile $rpt_temppath/report.05.html
 
 echo "GenerateReport - Merge trend data (all transactions)..."
 dotnet $rpt_toolspath/rpg.merge.dll jt project=$rpt_projectname category=trs valueindex=4 historycount=$rpt_maxtrendcount templatefile=$rpt_reporttemptargetfile workload=$rpt_workload database=$rpt_reportdbconnectstring
 if [ $? -ne 0 ]; then aborttest "merge transaction trenddata"; fi
-cp $rpt_reporttemptargetfile $rpt_temppath/report.06.html 
+cp -f $rpt_reporttemptargetfile $rpt_temppath/report.06.html 
 
 echo "GenerateReport - Merge trend data (all variables)..."
 dotnet $rpt_toolspath/rpg.merge.dll j project=$rpt_projectname category=var historycount=$rpt_maxtrendcount templatefile=$rpt_reporttemptargetfile workload=$rpt_workload database=$rpt_reportdbconnectstring
 if [ $? -ne 0 ]; then aborttest "merge variable trenddata"; fi
-cp $rpt_reporttemptargetfile $rpt_temppath/report.07.html
+cp -f $rpt_reporttemptargetfile $rpt_temppath/report.07.html
 
 echo "GenerateReport - Cleanup non-used variables rest of the report..."
 dotnet $rpt_toolspath/rpg.merge.dll v project=$rpt_projectname name=* value=" " templatefile=$rpt_reporttemptargetfile database=$rpt_reportdbconnectstring
-cp $rpt_reporttemptargetfile $rpt_temppath/report.08.html
+cp -f $rpt_reporttemptargetfile $rpt_temppath/report.08.html
 
 echo "Merge (data and html template) DONE"
