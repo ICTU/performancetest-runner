4d7a7d4fd40276b037001a4d7e08e91ffdd73a70
Marcel Wigman <mwigman@ymor.nl>
2020-02-11 14:52:01 +0000

    2 sql add-on scripts removed

--- 08_Database/update_value_category_entity.sql
+++
@@ -1,9 +0,0 @@
-update value set category='var', entity='trs' where key='transactionsfailed';
-update value set category='var', entity='trs' where key='transactionstotal';
-update value set category='var', entity='trs' where key='avgofaverages';
-update value set category='var', entity='trs' where key='avgof95percentiles';
-update value set category='var', entity='trs' where key='avgofmedians';
-update value set category='var', entity='trs' where key='avgof90percentiles';
-update value set category='var', entity='msr' where key='trendbreakstabilitypercentage';
-update value set category='var', entity='msr' where key='trendbreakrampupusers';
-update value set category='var', entity='msr' where key='trendbreakrampuppercentage';
