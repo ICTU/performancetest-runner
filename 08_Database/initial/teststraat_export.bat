39edbf809956918740f7f6c3b5a677f258a43e22
Marcel Wigman <mwigman@ymor.nl>
2020-02-11 14:52:54 +0000

    new database meta dump, script files enhanced

--- 08_Database/initial/teststraat_export.bat
+++ 08_Database/initial/teststraat_export.bat
@@ -1,12 +1,17 @@
+@echo off
+
 set pgdumppath="C:\Program Files (x86)\PostgreSQL\9.3\bin"
 
-set dumpfile="teststraat.dump"
+set dumpfile=teststraat.dump
 set dbname=teststraat
+set username=postgres
 
-echo Dump database %dbname% to %dumpfile%
+echo Dump database [%dbname%] to [%dumpfile%]
 
-rem Without data
-%pgdumppath%\pg_dump.exe --schema-only --format=c --port=5432 --host=localhost --username=postgres --file=%dumpfile% %dbname%
+rem No data, only structure/meta
+%pgdumppath%\pg_dump.exe --schema-only --format=c --port=5432 --host=localhost --username=%username% --file=%dumpfile% %dbname%
 
 rem With data
-rem %pgdumppath%\pg_dump.exe --format=c --port=5432 --host=localhost --username=postgres --file=%dumpfile% %dbname%
+rem %pgdumppath%\pg_dump.exe --format=c --port=5432 --host=localhost --username=%username% --file=%dumpfile% %dbname%
+
+echo Export is stored in file [%dumpfile%]
--- 08_Database/initial/teststraat_import.bat
+++ 08_Database/initial/teststraat_import.bat
@@ -7,11 +7,13 @@ set username=postgres
 set owner=postgres
 set dbname=teststraat
 
+SET /P password=Password for user [%username%]:
+
 echo Drop current database %dbname% (if any)...
-%pgbinpath%\dropdb  --host=%host% --port=%port% --username=%username% %dbname%
+%pgbinpath%\dropdb  --host=%host% --port=%port% --username=%username% --password=%password% %dbname%
 
 echo Create database %dbname%...
-%pgbinpath%\createdb --host=%host% --port=%port% --username=%username% --owner=%owner% %dbname%
+%pgbinpath%\createdb --host=%host% --port=%port% --username=%username% --owner=%owner% --password=%password% %dbname%
 
 echo Restore dump of database %dbname%...
-%pgbinpath%\pg_restore.exe --format=c --username=%username% --port=%port% --host=%host% --dbname=%dbname% --single-transaction --exit-on-error %dumpfile%
+%pgbinpath%\pg_restore.exe --format=c --username=%username% --password=%password% --port=%port% --host=%host% --dbname=%dbname% --single-transaction --exit-on-error %dumpfile%
